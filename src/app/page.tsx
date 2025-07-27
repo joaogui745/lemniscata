6da24b64a6ab2279bff94de1efbc8a2996522931
João Guilherme <76461118+joaogui745@users.noreply.github.com.>
2025-07-27 20:54:17 -0300

    Adicionando lemniscata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import Image from "next/image";
 
 export default function Home() {
   return (
-  <main>
-
-  </main>
+    <main className="min-h-screen bg-gray-100 flex items-center justify-center">
+          <h2 className="text-2xl font-semibold mb-4 text-gray-700">Unicode Symbol</h2>
+          <div className="text-8xl text-gradient font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600">
+            ∞
+          </div>
+    </main>
   );
 }
